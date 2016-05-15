a9a143add7fb3f6fe2e5382328b9bfbdecc8ee73
javierbravomx <javierbravomx@gmail.com>
2016-05-15 18:12:22 -0500

    Images
    Project images change

--- index.php
+++ index.php
@@ -12,7 +12,186 @@
     <link rel="stylesheet" href="css/body.css">
 </head>
 <body>
+    <nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="http://fca.unam.mx">
+                </a>
+            </div>
+        </div>
+    </nav>
     <div class="container">
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
+        <a>
+            <img class="img-responsive" src="">
+        </a>
     </div>
     <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
