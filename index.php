ee63f5a67b3b73025fff6dc92f867d40367f82ef
javierbravomx <javierbravomx@gmail.com>
2016-05-16 00:55:17 -0500

    More Sync
    Gallery bootstrap

--- index.php
+++ index.php
@@ -21,176 +21,164 @@
         </div>
     </nav>
     <div class="container">
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".actinver-detalle">
+            <img class="img-responsive" src="img/icon/actinver_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".ado-detalle">
+            <img class="img-responsive" src="img/icon/ado_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".amdaid-detalle">
+            <img class="img-responsive" src="img/icon/amdaid_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".amib-detalle">
+            <img class="img-responsive" src="img/icon/amib_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".aosport-detalle">
+            <img class="img-responsive" src="img/icon/aosport_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".asf-detalle">
+            <img class="img-responsive" src="img/icon/asf_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".aspel-detalle">
+            <img class="img-responsive" src="img/icon/aspel_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".baker-tilly-detalle">
+            <img class="img-responsive" src="img/icon/baker-tilly_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".bancomer-detalle">
+            <img class="img-responsive" src="img/icon/bancomer_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".bancomext-detalle">
+            <img class="img-responsive" src="img/icon/bancomext_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".banorte-ixe-detalle">
+            <img class="img-responsive" src="img/icon/banorte-ixe_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".bdo-detalle">
+            <img class="img-responsive" src="img/icon/bdo_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".bmv-detalle">
+            <img class="img-responsive" src="img/icon/bmv_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".bx-detalle">
+            <img class="img-responsive" src="img/icon/bx_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".canaco-detalle">
+            <img class="img-responsive" src="img/icon/canaco_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".ccpm-detalle">
+            <img class="img-responsive" src="img/icon/ccpm_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".conagua-detalle">
+            <img class="img-responsive" src="img/icon/conagua_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".contpaqi-detalle">
+            <img class="img-responsive" src="img/icon/contpaqi_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".deloitte-detalle">
+            <img class="img-responsive" src="img/icon/deloitte_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".despacho-asesoria-juridica-detalle">
+            <img class="img-responsive" src="img/icon/despacho-asesoria-juridica_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".donarte-detalle">
+            <img class="img-responsive" src="img/icon/donarte_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".e&y-detalle">
+            <img class="img-responsive" src="img/icon/e&y_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".ec-council-detalle">
+            <img class="img-responsive" src="img/icon/ec-council_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".emprebask-detalle">
+            <img class="img-responsive" src="img/icon/embrebask_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".fundacion-alfredo-harp-helu-detalle">
+            <img class="img-responsive" src="img/icon/fundacion-alfredo-harp-helu_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".genexus-detalle">
+            <img class="img-responsive" src="img/icon/genexus_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".gossler-detalle">
+            <img class="img-responsive" src="img/icon/gossler_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".grupo-modelo-detalle">
+            <img class="img-responsive" src="img/icon/grupo-modelo_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".hospital-infantil-de-mexico-detalle">
+            <img class="img-responsive" src="img/icon/hospital-infantil-de-mexico_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".ibm-detalle">
+            <img class="img-responsive" src="img/icon/ibm_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".ifecom-detalle">
+            <img class="img-responsive" src="img/icon/ifecom_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".imcp-detalle">
+            <img class="img-responsive" src="img/icon/imcp_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".imef-detalle">
+            <img class="img-responsive" src="img/icon/imef_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".inadem-detalle">
+            <img class="img-responsive" src="img/icon/inadem_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".inroads-detalle">
+            <img class="img-responsive" src="img/icon/inroads_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".instituto-para-el-fomento-a-la-calidad-total-detalle">
+            <img class="img-responsive" src="img/icon/instituto-para-el-fomento-a-la-calidad-total_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".issste-detalle">
+            <img class="img-responsive" src="img/icon/issste_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".kpmg-detalle">
+            <img class="img-responsive" src="img/icon/kpmg_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".latin-american-speakers-detalle">
+            <img class="img-responsive" src="img/icon/latin-american-speakers_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".loreal-detalle">
+            <img class="img-responsive" src="img/icon/loreal_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".manpower-detalle">
+            <img class="img-responsive" src="img/icon/manpower_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".maxcom-detalle">
+            <img class="img-responsive" src="img/icon/maxcom_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".nielsen-ibope-detalle">
+            <img class="img-responsive" src="img/icon/nielsen-ibope_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".principal-financial-group-detalle">
+            <img class="img-responsive" src="img/icon/principal-financial-group_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".procter&gamble-detalle">
+            <img class="img-responsive" src="img/icon/procter&gamble_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".prodecon-detalle">
+            <img class="img-responsive" src="img/icon/prodecon_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".pwcs-detalle">
+            <img class="img-responsive" src="img/icon/pwc_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".revistas-mexico-grupo-medios-detalle">
+            <img class="img-responsive" src="img/icon/revistas-mexico-grupo-medios_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".russell-bedford-detalle">
+            <img class="img-responsive" src="img/icon/russell-bedford_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".salles-sainz-grand-thorton-detalle">
+            <img class="img-responsive" src="img/icon/salles-sainz-grand-thorton_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".siemens-detalle">
+            <img class="img-responsive" src="img/icon/siemens_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".toks-detalle">
+            <img class="img-responsive" src="img/icon/toks_logo.png">
         </a>
-        <a>
-            <img class="img-responsive" src="">
-        </a>
-        <a>
-            <img class="img-responsive" src="">
-        </a>
-        <a>
-            <img class="img-responsive" src="">
-        </a>
-        <a>
-            <img class="img-responsive" src="">
-        </a>
-        <a>
-            <img class="img-responsive" src="">
+        <a class="col-md-1 thumbnail gallery" href=".unilever-detalle">
+            <img class="img-responsive" src="img/icon/unilever_logo.png">
         </a>
     </div>
     <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
