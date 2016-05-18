9099ccdb25e7e131d5cc5dce5756b1d0ee626feb
javierbravomx <javierbravomx@gmail.com>
2016-05-18 15:10:33 -0500

    IDK
    Still syncing to master, weird errors

--- index.php
+++ index.php
@@ -5,32 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Asignaturas Empresariales y Organizacionales</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/featherlight/1.4.1/featherlight.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/featherlight/1.4.1/featherlight.gallery.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+    <link href="//cdn.rawgit.com/noelboss/featherlight/1.4.0/release/featherlight.min.css" type="text/css" rel="stylesheet">
+    <link href="//cdn.rawgit.com/noelboss/featherlight/1.4.0/release/featherlight.gallery.min.css" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="css/body.css">
+	<link rel="stylesheet" href="css/detalle.css">
 </head>
 <body>
-    <nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="http://fca.unam.mx">
-                </a>
-            </div>
-        </div>
-    </nav>
-	<div class="container" id="main">
+	<div class="container main">
+        <div class="row main-row">
+		</div>
 	</div>
-    <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/featherlight/1.4.1/featherlight.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/featherlight/1.4.1/featherlight.gallery.min.js"></script>
-    <script>
-    $(document).ready(function(){
-        $('.gallery').featherlightGallery();
-    });
-    </script>
+    <script src="//cdn.rawgit.com/noelboss/featherlight/1.4.0/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="//cdn.rawgit.com/noelboss/featherlight/1.4.0/release/featherlight.gallery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script>
+	$(document).ready(function(){
+		$('.gallery').featherlightGallery();
+	});
+	</script>
 	<script type="text/javascript">
 	$(document).ready(function()
 	{
@@ -54,14 +50,24 @@
 				var asignatura_promedio=field.asignatura_promedio;
 				var asignatura_img=field.asignatura_img;
 
-				var rows = '<a class="col-md-1 thumbnail gallery"' + '>' +
-				'<img class="img-responsive" src="' + asignatura_img + '"></a>';
+				var rows = '<a class="col-md-1 thumbnail gallery"' + 'href=".' + asignatura_empresa.toLowerCase() + '-detalle">' +
+				'<img class="img-responsive" src="' + asignatura_img + '">' +
+				'</a>' +
+				'<div class="' + asignatura_empresa.toLowerCase() + '-detalle">' +
+				'<div class="row">' +
+				'<div class="col-md-1">' +
+				'</div>' +
+				'<div class="col-md-7">' +
+				'<h1>' + asignatura_empresa + '</h1>' +
+				'<h3>' + asignatura_asignatura + '</h3>' +
+				'</div>' +
+				'</div>' +
+				'</div>';
 
-				$('#main').append(rows);
+				$('.main-row').append(rows);
 	        });
     	});
 	});
-	</script>
 	</script>
 </body>
 </html>
