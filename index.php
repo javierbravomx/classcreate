63e1817c0cb924777458bad929c0dc6debc10d60
javierbravomx <javierbravomx@gmail.com>
2016-05-24 18:52:43 -0500

    99.999999999%
    lol

--- index.php
+++ index.php
@@ -184,7 +184,6 @@
             }
         });
     });
-
 	function check_carrera() {
 		if($('#administracion_checkbox').is(':checked')) {
 			$("img[data-carrera*='contaduria'][data-carrera*='administracion']").closest("a").fadeIn();
@@ -210,15 +209,171 @@
 		else {
 			$("img[data-carrera='informatica']").closest("a").fadeOut();
 		}
-
-		if(!$('#administracion_checkbox').is(':checked') && !$('#contaduria_checkbox').is(':checked') && !$('#informatica_checkbox').is(':checked')) {
-			$("img[data-carrera*='administracion']").closest("a").fadeIn();
-			$("img[data-carrera*='contaduria']").closest("a").fadeIn();
-			$("img[data-carrera*='informatica']").closest("a").fadeIn();
+	}
+	function check_dia() {
+		if($('#lunes_checkbox').is(':checked')) {
+			$("img[data-dia*='martes'][data-dia*='lunes']").closest("a").fadeIn();
+			$("img[data-dia*='miercoles'][data-dia*='lunes']").closest("a").fadeIn();
+			$("img[data-dia*='jueves'][data-dia*='lunes']").closest("a").fadeIn();
+			$("img[data-dia*='viernes'][data-dia*='lunes']").closest("a").fadeIn();
+			$("img[data-dia='lunes']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-dia*='lunes']").closest("a").fadeOut();
+		}
+		if($('#martes_checkbox').is(':checked')) {
+			$("img[data-dia*='lunes'][data-dia*='martes']").closest("a").fadeIn();
+			$("img[data-dia*='miercoles'][data-dia*='martes']").closest("a").fadeIn();
+			$("img[data-dia*='jueves'][data-dia*='martes']").closest("a").fadeIn();
+			$("img[data-dia*='viernes'][data-dia*='martes']").closest("a").fadeIn();
+			$("img[data-dia='martes']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-dia*='martes']").closest("a").fadeOut();
+		}
+		if($('#miercoles_checkbox').is(':checked')) {
+			$("img[data-dia*='martes'][data-dia*='miercoles']").closest("a").fadeIn();
+			$("img[data-dia*='lunes'][data-dia*='miercoles']").closest("a").fadeIn();
+			$("img[data-dia*='jueves'][data-dia*='miercoles']").closest("a").fadeIn();
+			$("img[data-dia*='viernes'][data-dia*='miercoles']").closest("a").fadeIn();
+			$("img[data-dia='miercoles']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-dia*='miercoles']").closest("a").fadeOut();
+		}
+		if($('#jueves_checkbox').is(':checked')) {
+			$("img[data-dia*='martes'][data-dia*='jueves']").closest("a").fadeIn();
+			$("img[data-dia*='miercoles'][data-dia*='jueves']").closest("a").fadeIn();
+			$("img[data-dia*='lunes'][data-dia*='jueves']").closest("a").fadeIn();
+			$("img[data-dia*='viernes'][data-dia*='jueves']").closest("a").fadeIn();
+			$("img[data-dia='jueves']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-dia*='jueves']").closest("a").fadeOut();
+		}
+		if($('#viernes_checkbox').is(':checked')) {
+			$("img[data-dia*='martes'][data-dia*='viernes']").closest("a").fadeIn();
+			$("img[data-dia*='miercoles'][data-dia*='viernes']").closest("a").fadeIn();
+			$("img[data-dia*='jueves'][data-dia*='viernes']").closest("a").fadeIn();
+			$("img[data-dia*='lunes'][data-dia*='viernes']").closest("a").fadeIn();
+			$("img[data-dia='viernes']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-dia*='viernes']").closest("a").fadeOut();
 		}
 	}
+	function check_hora() {
+		if($('#7_checkbox').is(':checked')) {
+			$("img[data-horario='7:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='7:00']").closest("a").fadeOut();
+		}
+		if($('#9_checkbox').is(':checked')) {
+			$("img[data-horario='9:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='9:00']").closest("a").fadeOut();
+		}
+		if($('#11_checkbox').is(':checked')) {
+			$("img[data-horario='11:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='11:00']").closest("a").fadeOut();
+		}
+		if($('#16_checkbox').is(':checked')) {
+			$("img[data-horario='16:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='16:00']").closest("a").fadeOut();
+		}
+		if($('#17_checkbox').is(':checked')) {
+			$("img[data-horario='17:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='17:00']").closest("a").fadeOut();
+		}
+		if($('#18_checkbox').is(':checked')) {
+			$("img[data-horario='18:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='18:00']").closest("a").fadeOut();
+		}
+		if($('#20_checkbox').is(':checked')) {
+			$("img[data-horario='20:00']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-horario*='20:00']").closest("a").fadeOut();
+		}
+	}
+	function check_semestre() {
+		if($('#3o_checkbox').is(':checked')) {
+			$("img[data-semestre='3o']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-semestre*='3o']").closest("a").fadeOut();
+		}
+		if($('#5o_checkbox').is(':checked')) {
+			$("img[data-semestre='5o']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-semestre*='5o']").closest("a").fadeOut();
+		}
+		if($('#7o_checkbox').is(':checked')) {
+			$("img[data-semestre='7o']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-semestre*='7o']").closest("a").fadeOut();
+		}
+	}
+	function check_tipo() {
+		if($('#prof_checkbox').is(':checked')) {
+			$("img[data-tipo='profesionalizante']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-tipo*='profesionalizante']").closest("a").fadeOut();
+		}
+		if($('#comp_checkbox').is(':checked')) {
+			$("img[data-tipo='complementaria']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-tipo*='complementaria']").closest("a").fadeOut();
+		}
+	}
+	function check_salon() {
+		if($('#fca_checkbox').is(':checked')) {
+			$("img[data-salon!='emp']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-salon!='emp']").closest("a").fadeOut();
+		}
+		if($('#emp_checkbox').is(':checked')) {
+			$("img[data-salon*='emp']").closest("a").fadeIn();
+		}
+		else {
+			$("img[data-salon*='emp']").closest("a").fadeOut();
+		}
+	}
+
 	$(":checkbox").change(function(){
-		check_carrera();
+		if($(this).attr("name") == "carrera") {
+			check_carrera();
+		}
+		if($(this).attr("name") == "dia") {
+			check_dia();
+		}
+		if($(this).attr("name") == "hora") {
+			check_hora();
+		}
+		if($(this).attr("name") == "semestre") {
+			check_semestre();
+		}
+		if($(this).attr("name") == "tipo") {
+			check_tipo();
+		}
+		if($(this).attr("name") == "salon") {
+			check_salon();
+		}
     });
     </script>
     <script>
